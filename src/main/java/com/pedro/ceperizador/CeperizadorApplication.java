8ba89ec3555504dbc4331b81b8ec077c8787eb0f
Pedro Almodovar Pinto <pedroalmodovar94@gmail.com>
2021-02-19 15:32:46 -0300

    Projeto criado

---
+++ src/main/java/com/pedro/ceperizador/CeperizadorApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.ceperizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeperizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeperizadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/ceperizador/CeperizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.ceperizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeperizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
