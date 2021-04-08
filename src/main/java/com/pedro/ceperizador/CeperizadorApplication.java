03efc4ab7bc381dc1e84a8c9e3dafe110d56ea54
Pedro <pedroalmodovar94@gmail.com>
2021-04-08 00:30:06 -0300

    Adicionado deploy com tomcat utilizando war

--- src/main/java/com/pedro/ceperizador/CeperizadorApplication.java
+++ src/main/java/com/pedro/ceperizador/CeperizadorApplication.java
@@ -2,14 +2,20 @@ package com.pedro.ceperizador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class CeperizadorApplication {
+public class CeperizadorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeperizadorApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CeperizadorApplication.class);
+	}
 }
